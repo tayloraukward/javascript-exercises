9329569aa2ecf379c624ac49bbeca15327792989
Taylor Aukward <tja8we@virginia.edu>
2022-08-04 14:54:18 -0400

    completed the js code for lesson 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if ((typeof(num1) != 'number' || typeof(num2) != 'number')|| (num1 < 0 || num2 < 0)) return 'ERROR';
+    else {
+        if (num1 <= num2){
+            for (num1; num1 <= num2; num1++){
+                sum += num1;
+            }
+            return sum;
+        }
+        else {
+            for (num1; num1 >= num2; num1--){
+                sum += num1;
+            }
+            return sum;
+        }
+    }
 
 };
 
