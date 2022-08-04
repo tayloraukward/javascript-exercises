cde5a10dea7f702280712c9eda01370d6541a53f
Taylor Aukward <tja8we@virginia.edu>
2022-08-04 14:43:42 -0400

    finished lesson 4 js code

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,20 +1,8 @@
 const removeFromArray = function(arr, ...args) {
-    if (args.length == undefined) return arr;
-    else if (args.length === 1) {
-        let final = arr.filter(function(value){
-            return value != args[0];
-        });
-        return final;
-    }
-    else {
-        let final2 = arr.filter(function(value){
-            for (let i = 0; i < args.length; i++){
-                return value != args[i];
-            }
-        });
-        return final2;
-    }
-};
+    return arr.filter(function(val) {
+        return !args.includes(val);
+    } );
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
