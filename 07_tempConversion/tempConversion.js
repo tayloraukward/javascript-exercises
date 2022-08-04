10f3b6a59fa03d8f2de90bb4f818993b977ed92e
Taylor Aukward <tja8we@virginia.edu>
2022-08-04 15:08:04 -0400

    finsihed lesson 7 of the javascript exercises which wraps up the first Odin Project lesson.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+    let num = (5/9)* (temp - 32);
+    return Math.round(num * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+    let num = (temp * 1.8) + 32;
+    return Math.round(num * 10) / 10
 };
 
 // Do not edit below this line
